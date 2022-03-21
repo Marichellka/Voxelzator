bbea6347b7ef79fc69443995904a93f473641687
Andras <dlink.uaua@gmail.com>
2022-03-21 16:50:13 +0200

    fixed naming

--- Voxelzator/Voxelzator/Solver.cs
+++ Voxelzator/Voxelzator/Solver.cs
@@ -8,30 +8,30 @@ namespace Voxelzator
 {
     public class Solver
     {
-        private const double eps = 1e-7;
+        private const double Eps = 1e-7;
         class LinePasser
         {
-            private Point currentPoint;
-            private Point lastPoint;
-            private double xGridStep;
-            private double yGridStep;
-            private double xNextBorder;
-            private double yNextBorder;
-            private double dx;
-            private double dy;
+            private readonly Point _currentPoint;
+            private readonly Point _lastPoint;
+            private readonly double _xGridStep;
+            private readonly double _yGridStep;
+            private double _xNextBorder;
+            private double _yNextBorder;
+            private readonly double _dx;
+            private readonly double _dy;
             public LinePasser(Point start, Point finish, double step)
             {
-                currentPoint = start;
-                lastPoint = finish;
-                dx = finish.x - start.x;
-                dy = finish.y - start.y;
+                _currentPoint = start;
+                _lastPoint = finish;
+                _dx = finish.x - start.x;
+                _dy = finish.y - start.y;
                 
-                xGridStep = finish.x > start.x ? step : -step;
-                yGridStep = finish.y > start.y ? step : -step;
+                _xGridStep = finish.x > start.x ? step : -step;
+                _yGridStep = finish.y > start.y ? step : -step;
 
-                //TODO: figure out wether step shouldn't be replaced with xGridStep
-                xNextBorder = (Math.Round((start.x - xGridStep / 2) / xGridStep) + 0.5) * xGridStep + xGridStep / 2;
-                yNextBorder = (Math.Round((start.y - yGridStep / 2) / yGridStep) + 0.5) * yGridStep + yGridStep / 2;
+                //TODO: figure out whether step shouldn't be replaced with xGridStep
+                _xNextBorder = (Math.Round((start.x - _xGridStep / 2) / _xGridStep) + 0.5) * _xGridStep + _xGridStep / 2;
+                _yNextBorder = (Math.Round((start.y - _yGridStep / 2) / _yGridStep) + 0.5) * _yGridStep + _yGridStep / 2;
             }
 
             static double LengthTillBorder(double current, double border, double speed)
@@ -41,25 +41,25 @@ namespace Voxelzator
             public Point NextStep()
             {
                 
-                if ((lastPoint.x - currentPoint.x) * dx <= eps || (lastPoint.y - currentPoint.y) * dy <= eps)
+                if ((_lastPoint.x - _currentPoint.x) * _dx <= Eps || (_lastPoint.y - _currentPoint.y) * _dy <= Eps)
                     return null;
 
-                var returnPoint = new Point(xNextBorder - xGridStep / 2, yNextBorder - yGridStep / 2);
+                var returnPoint = new Point(_xNextBorder - _xGridStep / 2, _yNextBorder - _yGridStep / 2);
                 
-                double xOffset = LengthTillBorder(currentPoint.x, xNextBorder, dx);
-                double yOffset = LengthTillBorder(currentPoint.y, yNextBorder, dy);
+                double xOffset = LengthTillBorder(_currentPoint.x, _xNextBorder, _dx);
+                double yOffset = LengthTillBorder(_currentPoint.y, _yNextBorder, _dy);
 
                 if (xOffset < yOffset)
                 {
-                    currentPoint.x = xNextBorder;
-                    xNextBorder += xGridStep;
-                    currentPoint.y += xOffset * dy;
+                    _currentPoint.x = _xNextBorder;
+                    _xNextBorder += _xGridStep;
+                    _currentPoint.y += xOffset * _dy;
                 }
                 else
                 {
-                    currentPoint.y = yNextBorder;
-                    yNextBorder += yGridStep;
-                    currentPoint.x += yOffset * dx;
+                    _currentPoint.y = _yNextBorder;
+                    _yNextBorder += _yGridStep;
+                    _currentPoint.x += yOffset * _dx;
                 }
                 
                 return returnPoint;
@@ -74,12 +74,13 @@ namespace Voxelzator
             Array.Sort(triangle, new Comparison<Point>((lhs, rhs) => lhs.x.CompareTo(rhs.x)));
 
             var mainLinePasser = new LinePasser(triangle[0], triangle[2], step);
+            var slaveLinePasser = new LinePasser(triangle[0], triangle[1], step);
             
-            for(Point cube = mainLinePasser.NextStep(); cube != null; cube = mainLinePasser.NextStep())
-                result.Add(cube);
-            
-            
-            
+            for (Point mainCube = mainLinePasser.NextStep(), slaveCube = slaveLinePasser.NextStep(); mainCube != null; mainCube = mainLinePasser.NextStep())
+            {
+                result.Add(mainCube);
+            }
+
             return result;
         }
     }
