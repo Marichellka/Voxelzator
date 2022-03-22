7dd0ca90342106ff348788556a9039926d6c90b5
Andras <dlink.uaua@gmail.com>
2022-03-22 19:06:14 +0200

    fixed edges parralel to axis

--- Pixelzator/Library/LinePasser.cs
+++ Pixelzator/Library/LinePasser.cs
@@ -38,11 +38,24 @@ namespace Library
 
             public Point? NextStep()
             {
-                if ((_lastPoint.x - _currentPoint.x) * _dx <= Solver.Eps || (_lastPoint.y - _currentPoint.y) * _dy <= Solver.Eps)
+                if ((_lastPoint.x - _currentPoint.x) * _dx <= Solver.Eps && (_lastPoint.y - _currentPoint.y) * _dy <= Solver.Eps)
                     return null;
 
                 var returnPoint = new Point(_xNextBorder - _xGridStep / 2, _yNextBorder - _yGridStep / 2);
 
+                if (Math.Abs(_dx) < Solver.Eps)
+                {
+                    _currentPoint.y = _yNextBorder;
+                    _yNextBorder += _yGridStep;
+                    return returnPoint;
+                }
+                if (Math.Abs(_dy) < Solver.Eps)
+                {
+                    _currentPoint.x = _xNextBorder;
+                    _xNextBorder += _xGridStep;
+                    return returnPoint;
+                }
+                
                 double xOffset = LengthTillBorder(_currentPoint.x, _xNextBorder, _dx);
                 double yOffset = LengthTillBorder(_currentPoint.y, _yNextBorder, _dy);
 
