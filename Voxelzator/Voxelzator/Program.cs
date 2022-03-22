dcb356ed4d323e06ec7d96642b6854c74f620d19
Andras <dlink.uaua@gmail.com>
2022-03-22 18:33:47 +0200

    added axis output

--- Voxelzator/Voxelzator/Program.cs
+++ Voxelzator/Voxelzator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Voxelzator
 {
@@ -6,14 +7,27 @@ namespace Voxelzator
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.Default;
             Solver solver = new Solver();
-            Point[] triangle = new Point[] { new Point(1, 1), new Point(1, 6), new Point(4, 1)};
-            var list = solver.CreateListOfRectangles(triangle, 0.5);
+            Point[] triangle = new Point[] { new Point(0, 0), new Point(2, 6), new Point(4, 1)};
+            var list = solver.CreateListOfRectangles(triangle, 1);
             Console.WriteLine(list.Count);
+            
+            int[,] grid = new int[10, 10];
             
             foreach (Point cube in list)
             {
+                grid[(int) cube.y, (int) cube.x] = 1;
                 Console.WriteLine(cube);
+            }
+            
+            for (int i = 9; i >= 0; i--)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(grid[i,j]==0?"⬜️":"⬛️");
+                }
+                Console.WriteLine();
             }
         }
     }
