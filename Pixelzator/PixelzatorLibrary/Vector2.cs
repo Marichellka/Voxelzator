c6f61bd7005aa2962c093f32d9b2bf1cdc0e27f1
Marichka <marichka.v.richtsi@gmail.com>
2022-03-22 19:03:25 +0100

    add vector2 class

---
+++ Pixelzator/PixelzatorLibrary/Vector2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PixelzatorLibrary
+{
+    public class Vector2
+    {
+        public double X { get; private set; }
+        public double Y { get; private set;}
+        public double Length { get; }
+
+        public Vector2(Point v1, Point v2)
+        {
+            X = v2.X - v1.X;
+            Y = v2.Y - v1.Y;
+            Length = Math.Sqrt(X * X + Y * Y);
+        }
+
+        public void Normalize()
+        {
+            X /= Length;
+            Y /= Length;
+        }
+    }
+}
